fd2b080a21bd4d5d39e95390b053f7a912ddcd27
DiegoHidalgo02 <hidalgo.moreno.diego@gmail.com>
2025-01-15 03:10:54 +0100

    Enhance modal functionality by creating a container for SVG icons and updating project type display

--- js/projectGallery.js
+++ js/projectGallery.js
@@ -4,7 +4,8 @@ function createSvgIcons(card) {
     const techArray = card.dataset.technologies.split(",");
     const svgNS = "http://www.w3.org/2000/svg"; // Aggiungi namespace SVG
     const xlinkNS = "http://www.w3.org/1999/xlink"; // Aggiungi namespace xlink
-
+    const modal_technologies = document.querySelector("#modal-technologies");
+    const modal_technologies_svg_container = document.createElement("div");
     techArray.forEach(element => {
         // Create SVG
         const svg = document.createElementNS(svgNS, "svg");
@@ -15,24 +16,42 @@ function createSvgIcons(card) {
         use.setAttributeNS(xlinkNS, "xlink:href", `#icon-${element}`);
 
         svg.appendChild(use);
+
+        modal_technologies_svg_container.append(svg)
         
-        document.querySelector("#modal-technologies").appendChild(svg)
+        modal_technologies.append(modal_technologies_svg_container);
+
+
     });
 }
+
+const project = document.createElement("span");
 
 gallery_container.addEventListener("click", (e) => {
 
     const button =  e.target.classList.contains("button")
     const card = e.target.parentNode;
+    const modal_technologies = document.querySelector("#modal-technologies");
+    const projectTypeText = document.querySelector("#ProjectType");
+
 
     if (button){
         const modal_title = document.querySelector("#exampleModalLongTitle");
         modal_title.textContent = card.dataset.title
 
+        project.innerHTML = "";
+        project.textContent = card.dataset.projectype;
+        projectTypeText.append(project);
+
         const modal_img = document.querySelector(".card-img-top")
         modal_img.src = card.dataset.img;
 
-        document.querySelector("#modal-technologies").innerHTML = '';
+        modal_technologies.innerHTML = '';
+
+        const technologiesTitle = document.createElement("p")
+        technologiesTitle.textContent = "Technologies";
+        modal_technologies.append(technologiesTitle);
+        
         createSvgIcons(card);
         
         const modal_description = document.querySelector("#modal-description")
